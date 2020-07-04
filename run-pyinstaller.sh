d8420c849c31cad2db4ce9206a3d767ba9717685
Gil Gonen <ggonen@synamedia.com>
2020-07-04 23:45:59 +0300

    Changes for supporting exe made with pyinstaller

--- run-pyinstaller.sh
+++ run-pyinstaller.sh
@@ -1,6 +1,6 @@
 
-rm -rf dist/*
+rm -rf dist/* build/*
 
 pipenv run pyinstaller L1L2_subs.spec
 
-/c/Program\ Files/7-Zip/7z a -r dist/L1L2_subs.zip dist/L1L2_subs
+#/c/Program\ Files/7-Zip/7z a -r dist/L1L2_subs.zip dist/L1L2_subs
