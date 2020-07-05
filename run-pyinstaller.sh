813c5d537af0832283fda571760d5a8de0727041
Gil Gonen <ggonen@synamedia.com>
2020-07-05 23:51:50 +0300

    script to run pyinstaller in windows

--- run-pyinstaller.sh
+++ run-pyinstaller.sh
@@ -3,4 +3,4 @@ rm -rf dist/* build/*
 
 pipenv run pyinstaller L1L2_subs.spec
 
-#/c/Program\ Files/7-Zip/7z a -r dist/L1L2_subs.zip dist/L1L2_subs
+/c/Program\ Files/7-Zip/7z a -r dist/L1L2_subs.zip dist/L1L2_subs
